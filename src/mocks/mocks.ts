3b5d869507a384ab5357185428843d6d5f17351a
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-08-12 11:23:05 +0500

    test: add mock users

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,40 @@
+import type { Users } from '@/types/types';
+
+export const mockUsers: Users = [
+  {
+    id: 1,
+    name: 'Ivan',
+    email: 'ivan.petrov@example.com',
+    role: 'admin',
+  },
+  {
+    id: 2,
+    name: 'Anna Sidorova',
+    email: 'anna.sidorova@example.com',
+    role: 'user',
+  },
+  {
+    id: 3,
+    name: 'Сергей Иванов',
+    email: 'sergey.ivanov@example.com',
+    role: 'manager',
+  },
+  {
+    id: 4,
+    name: 'Elena Kuznecova',
+    email: 'elena.kuznetsova@example.com',
+    role: 'user',
+  },
+  {
+    id: 5,
+    name: 'Дмитрий Смирнов',
+    email: 'dmitry.smirnov@example.com',
+    role: 'admin',
+  },
+  {
+    id: 6,
+    name: 'Ольга Васильева',
+    email: 'olga.vasilyeva@example.com',
+    role: 'user',
+  },
+];
